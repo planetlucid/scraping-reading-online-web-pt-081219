3b6c4ca791e93c8f5106c8d378dbcdd3c85465ce
Ryan S <planetlucid@gmail.com>
2019-09-26 18:00:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
-
+ 
 html = open("https://flatironschool.com/")
-
-Nokogiri::HTML(html)
-
 doc = Nokogiri::HTML(html)
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-26OIBN")
-doc.css(".headline-26OIBN").text
-puts doc.css(".headline-26OIBN")
+ 
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
